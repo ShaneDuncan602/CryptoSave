c844c45e731e6d38faa9bfc97b630d6c7803e315
Shane Duncan <106779100+ShaneDuncan602@users.noreply.github.com>
2022-12-05 14:55:18 -0600

    stubbed CryptoSave

--- src/CryptoSave.sol
+++ src/CryptoSave.sol
@@ -1,9 +1,24 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.17;
+import "openzeppelin-contracts/access/Ownable.sol";
+import "openzeppelin-contracts/token/ERC20/IERC20.sol";
 
-contract CryptoSave {
-    uint8 public percentRise;
-    uint8 public percentFall;
+contract CryptoSave is Ownable {
+    uint8 public percentRise = 10;
+    uint8 public percentFall = 10;
+    uint256 public lastStablePrice;
+    uint256 public lastCryptoPrice;
+
+    ///This function is called from the external timer and is the entry point
+    function poke() external returns (bool success) {
+        //check oracle for spot price
+    }
+
+    /// Trade contract assets to stable coin
+    function tradeToStable() internal {}
+
+    /// Trade contract assets to crypto coin
+    function tradeToCrypto() internal {}
 
     function setPercentRise(uint8 _percentRise) public {
         percentRise = _percentRise;
