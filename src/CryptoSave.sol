e28ee22888a68572b60a7f4da3ca3b80b83d3ab8
Shane Duncan <106779100+ShaneDuncan602@users.noreply.github.com>
2022-12-05 15:03:02 -0600

    more cryptosave stubs

--- src/CryptoSave.sol
+++ src/CryptoSave.sol
@@ -8,6 +8,8 @@ contract CryptoSave is Ownable {
     uint8 public percentFall = 10;
     uint256 public lastStablePrice;
     uint256 public lastCryptoPrice;
+    uint256 cryptoAmount;
+    uint256 stableAmount;
 
     ///This function is called from the external timer and is the entry point
     function poke() external returns (bool success) {
@@ -20,11 +22,37 @@ contract CryptoSave is Ownable {
     /// Trade contract assets to crypto coin
     function tradeToCrypto() internal {}
 
+    // Setter for the percent in rise that will trigger the trade from stable to crypto
     function setPercentRise(uint8 _percentRise) public {
         percentRise = _percentRise;
     }
 
+    // Setter for the percent in fall that will trigger the trade from crypto to stable
     function setPercentFall(uint8 _percentFall) public {
         percentFall = _percentFall;
+    }
+
+    /**
+     * @dev transfer all the tokens from the address of this contract
+     * to address of the owner
+     */
+    function cashOut() external onlyOwner {
+        /// transfer stable coin to the owner (TODO: add dai address)
+        withdrawToken(address(0), stableAmount);
+        /// transfer crypto to the owner (TODO: add eth address)
+        withdrawToken(address(0), cryptoAmount);
+    }
+
+    /**
+     * @dev transfer the token from the address of this contract
+     * to address of the owner
+     */
+    function withdrawToken(address _tokenContract, uint256 _amount) internal {
+        IERC20 tokenContract = IERC20(_tokenContract);
+
+        // needs to execute `approve()` on the token contract to allow itself the transfer
+        tokenContract.approve(address(this), _amount);
+
+        tokenContract.transferFrom(address(this), owner(), _amount);
     }
 }
